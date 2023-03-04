74f2d11e1ecbe4b380cb5c12f060af897092fad3
Ruaaa <123161972+Ruaaa1983@users.noreply.github.com>
2023-03-05 02:46:37 +0800

    Update truffle.js

--- truffle.js
+++ truffle.js
@@ -2,9 +2,9 @@ module.exports = {
   networks: {
     development: {
       host: 'localhost',
-      port: 8545,
+      port: 8080,
       network_id: '*',
-      gas: 2500
+      gas: 3500
     }
   }
 };
